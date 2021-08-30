0bbf6711b142663f21811cd424849af767afd1f8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-08-30 17:37:06 +0200

    Update audio_editor.py

--- audio_editor.py
+++ audio_editor.py
@@ -6,8 +6,13 @@ from pydub import AudioSegment
 class editor():
     def __init__(self):
         self.root = Tk()
-        self.root.geometry('920x410')
+        self.root.geometry('923x410')
         self.root.title("Audio Editor")
+
+        self.currentDir = StringVar()
+        self.currentDir.set(os.getcwd())
+
+        Entry(self.root,textvariable=self.currentDir,width=153).place(x=0,y=0)
 
         self.root.mainloop()
 
