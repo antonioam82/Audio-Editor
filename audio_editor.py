07c3559a3f21fe2e68016bcbf2f1898cb5680370
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-08-31 18:53:38 +0200

    Update audio_editor.py

--- audio_editor.py
+++ audio_editor.py
@@ -12,11 +12,14 @@ class editor():
         self.currentDir = StringVar()
         self.currentDir.set(os.getcwd())
         self.audioName = StringVar()
+        self.duration = IntVar()
 
         Entry(self.root,textvariable=self.currentDir,width=153).place(x=0,y=0)
         Label(self.root,text="AUDIO TITLE").place(x=10,y=30)
         self.entryName = Entry(self.root,textvariable=self.audioName,width=40,font=('arial 20'))
         self.entryName.place(x=10,y=53)
+        self.durationEntry = Entry(self.root,textvariable=self.duration,width=13,font=('arial 20')).place(x=690,y=53)
+        Label(self.root,text="DURATION").place(x=690,y=30)
 
         self.root.mainloop()
 
