ff07ed853fb562fe07281db099d001a8178bb864
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-09-02 16:41:24 +0200

    Update audio_editor.py

--- audio_editor.py
+++ audio_editor.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 import os
 import threading
 from pydub import AudioSegment
@@ -20,9 +21,18 @@ class editor():
         self.entryName.place(x=10,y=53)
         self.durationEntry = Entry(self.root,textvariable=self.duration,width=13,font=('arial 20')).place(x=690,y=53)
         Label(self.root,text="DURATION").place(x=690,y=30)
-        Button(self.root,text="SEARCH AUDIO FILE",width=85,height=2).place(x=10,y=100)
+        Button(self.root,text="SEARCH AUDIO FILE",width=85,height=2,command=self.open_file).place(x=10,y=100)
 
         self.root.mainloop()
+
+    def open_file(self):
+        audio_file = filedialog.askopenfilename(initialdir = "/",
+                     title="Select audio",filetypes = (("mp3 files","*.mp3"),
+                     ("wav files","*.wav")))
+        if audio_file != "":
+            audio_f = (audio_file.split("/"))[-1]
+            self.audioName.set(audio_f)
+        
 
 
 if __name__=="__main__":
